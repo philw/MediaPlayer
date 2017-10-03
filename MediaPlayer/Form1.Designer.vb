c45ec35befbd821c141aefcc642e29b0425e76c3
Phil Wood <phil@pwood.net>
2017-10-04 00:33:28 +0100

    First version

--- MediaPlayer/Form1.Designer.vb
+++ MediaPlayer/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class MainForm
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,9 +22,57 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
+        Me.AxWindowsMediaPlayer1 = New AxWMPLib.AxWindowsMediaPlayer()
+        Me.PlayButton = New System.Windows.Forms.Button()
+        Me.StopButton = New System.Windows.Forms.Button()
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'AxWindowsMediaPlayer1
+        '
+        Me.AxWindowsMediaPlayer1.Enabled = True
+        Me.AxWindowsMediaPlayer1.Location = New System.Drawing.Point(12, 12)
+        Me.AxWindowsMediaPlayer1.Name = "AxWindowsMediaPlayer1"
+        Me.AxWindowsMediaPlayer1.OcxState = CType(resources.GetObject("AxWindowsMediaPlayer1.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxWindowsMediaPlayer1.Size = New System.Drawing.Size(138, 48)
+        Me.AxWindowsMediaPlayer1.TabIndex = 0
+        Me.AxWindowsMediaPlayer1.Visible = False
+        '
+        'PlayButton
+        '
+        Me.PlayButton.Location = New System.Drawing.Point(153, 226)
+        Me.PlayButton.Name = "PlayButton"
+        Me.PlayButton.Size = New System.Drawing.Size(75, 23)
+        Me.PlayButton.TabIndex = 1
+        Me.PlayButton.Text = "Play"
+        Me.PlayButton.UseVisualStyleBackColor = True
+        '
+        'StopButton
+        '
+        Me.StopButton.Location = New System.Drawing.Point(48, 226)
+        Me.StopButton.Name = "StopButton"
+        Me.StopButton.Size = New System.Drawing.Size(75, 23)
+        Me.StopButton.TabIndex = 2
+        Me.StopButton.Text = "Stop"
+        Me.StopButton.UseVisualStyleBackColor = True
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.StopButton)
+        Me.Controls.Add(Me.PlayButton)
+        Me.Controls.Add(Me.AxWindowsMediaPlayer1)
+        Me.Name = "MainForm"
+        Me.Text = "Media Player"
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents AxWindowsMediaPlayer1 As AxWMPLib.AxWindowsMediaPlayer
+    Friend WithEvents PlayButton As Button
+    Friend WithEvents StopButton As Button
 End Class
--- MediaPlayer/Form1.vb
+++ MediaPlayer/Form1.vb
@@ -1,3 +1,13 @@
-﻿Public Class Form1
+﻿Public Class MainForm
+    Private Sub PlayButton_Click(sender As Object, e As EventArgs) Handles PlayButton.Click
 
+        AxWindowsMediaPlayer1.URL = "C:\Users\Phil\Dropbox\strode\_A_LEVEL_CS\A2Projects\midi\teddybear.mid"
+
+    End Sub
+
+    Private Sub StopButton_Click(sender As Object, e As EventArgs) Handles StopButton.Click
+
+        AxWindowsMediaPlayer1.Ctlcontrols.stop()
+
+    End Sub
 End Class
